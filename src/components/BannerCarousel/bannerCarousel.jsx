9da97d5ccde60c2014d4d808cda8ae4ca63422bf
Tech-nanisai <techaettarisai@gmail.com>
2026-02-12 16:21:02 +0530

    Fix case sensitivity for BannerCarousel.css import to resolve Vercel build error

--- src/components/BannerCarousel/bannerCarousel.jsx
+++ src/components/BannerCarousel/bannerCarousel.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaChevronLeft, FaChevronRight, FaArrowRight, FaMagic } from "react-icons/fa";
-import "./BannerCarousel.css";
+import "./bannerCarousel.css";
 
 const slides = [
   {
